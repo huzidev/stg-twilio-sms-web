b3427b7d90cfa8e7abb65854a1ef1ccc23ed396f
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2025-06-18 01:38:50 +0500

    regex pattern updated

--- src/js/util.js
+++ src/js/util.js
@@ -18,5 +18,6 @@ export const copyToClipboard = s =>
     .then(() => console.log("Text copied", s))
     .catch(() => console.log("Unable to copy to clipboard"))
 
-// Starts with plus followed by at least 11 digits
-export const phonePattern = "^\\+\\d{11,}$"
+// Starts with plus followed by at least 10 to 15 digits
+// export const phonePattern = "^\\+\\d{11,}$"
+export const phonePattern = "^\\+\\d{10,15}$";
